a5a0fd0ccaedbae513196a61a11152cb2ad5e60b
Miqueas Guerrero <129316828+Miqueas0@users.noreply.github.com>
2023-06-28 19:56:48 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import { Divider, Alert, TextField} from '@mui/material';
+
+function App() {
+  const [text, setText] = useState('');
+
+  const [wordCount, setWordCount] = useState(0);
+
+  const [charCount, setCharCount] = useState(0);
+
+  const changeHandler = (e) => {
+    setText(e.target.value);
+  };
+
+  useEffect(() => {
+    const words = text.split(' ');
+
+    let wordCount = 0;
+    words.forEach((word) => {
+      if (word.trim() !== '') {
+        wordCount++;
+      }
+    });
+    setWordCount(wordCount);
+
+    setCharCount(text.length);
+  }, [text]);
+
+  return (
+    <Divider className='container'>
+      
+      <Divider>
+        <br></br>
+        <TextField
+        className='bar'
+          style={{background: "#ddd0c2"}}
+          maxLength="100"
+          value={text}
+          onChange={changeHandler}
+          placeholder='¿Que piensas?'
+        ></TextField>
+       
+      <p>{text}</p>
+      
+        <Divider className="progress">
+          <form>{100 - text.length} caracteres restantes.</form>
+          <Divider>
+              {(() => {
+                if (100 - text. length == 0) {
+                  
+                  return (
+                    Alert("Limite Alcanzado")
+                    
+                  )
+                  
+                }
+              })()}
+          </Divider>
+
+        <Divider>
+          <p className='word-count'>Palabras: {wordCount}</p>
+          <p className='char-count'>Caracteres: {charCount}</p>
+        </Divider>
+      </Divider>
+    </Divider>
+    </Divider>
+  );
+}
+
+export default App;
